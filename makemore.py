ac5a0affcc9df63df719c8c14243f14053ea3279
markus hector <markus.hector@visigon.com>
2024-04-13 13:39:22 +0200

    Fixed gitignore-file and pushed standard bigram model to repo

---
+++ makemore.py
@@ -0,0 +1,98 @@
+import torch
+from typing import List
+
+
+class Bigram:
+    """
+    Expects a .txt file as input, containing for each row in the
+    file, a datapoint.
+    """
+
+    def __init__(self, file, smoothing=0):
+        self.file: str = file
+        self.smoothing = smoothing
+
+        self.chars = None
+        self.s2i = None
+        self.i2s = None
+
+        self.N = None
+        self.P = None
+        self.one = None
+
+        self._parse_file()
+
+    def _parse_file(self):
+        words = open(self.file, 'r').read().splitlines()
+
+        self.chars = ['.'] + sorted(list(set(''.join(words))))
+        self.s2i = {s: i for i, s in enumerate(self.chars)}
+        self.i2s = {i: s for i, s in enumerate(self.chars)}
+
+        n = len(self.chars)
+        self.N = torch.zeros((n, n), dtype=torch.int32)
+        self.one = torch.ones_like(self.N).float()
+        self.one /= self.one.sum(1, keepdim=True)
+
+        for w in words:
+            ch = ['.'] + list(w) + ['.']
+            for c1, c2 in zip(ch, ch[1:]):
+                i = self.s2i[c1]
+                j = self.s2i[c2]
+                self.N[i, j] += 1
+
+        # Smoothing
+        self.N += self.smoothing
+
+        self.P = self.N / self.N.sum(1, keepdim=True)
+
+    def generate(self, n: int, random: bool = False):
+        """
+        With random set to True, returns an output generated from
+        a uniform distribution.
+        """
+        names = {}
+        for i in range(n):
+
+            g = torch.Generator().manual_seed(2147483647)
+
+            word = '.'
+            idx = self.s2i[word]
+            count = 0
+            while True:
+                p = self.P[idx] if not random else self.one[idx].float()
+
+                idx = torch.multinomial(p, num_samples=1, replacement=True).item()
+                cha = self.i2s[idx]
+                word += cha
+                if cha == '.' or count == 100:
+                    break
+                count += 1
+            names[i] = word
+        return names
+
+    def _get_cond_prob(self, char: str, given: str):
+        i = self.s2i[given]
+        j = self.s2i[char]
+        cond_prob = self.P[i, j]
+        return cond_prob
+
+    def log_likelihood(self, words, random=False):
+        if type(words) == str:
+            words = [words]
+        n = 0
+        log_likelihood = 0.0
+        for w in words:
+            ch = ['.'] + list(w) + ['.']
+            for c1, c2 in zip(ch, ch[1:]):
+                i = self.s2i[c1]
+                j = self.s2i[c2]
+                logprob = torch.log(self.P[i, j]) if not random else torch.log(self.one[i, j])
+                log_likelihood += logprob
+                n += 1
+        return log_likelihood / n
+
+    def __repr__(self):
+        i, j = self.N.shape
+        return f"Bigram-model of token size: ({i}x{j}), smoothing: {self.smoothing}"
+
